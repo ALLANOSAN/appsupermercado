08b099e4325be003e6e5837052595d1f9611e904
ALLANOSAN <allanstryper@live.com>
2024-06-13 13:51:44 -0300

    appmercadinho

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,40 @@
+import 'package:appsupermercado/screens/main_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
+import 'providers/cart_provider.dart';
 import 'providers/category_provider.dart';
 import 'providers/product_provider.dart';
-import 'providers/cart_provider.dart';
 import 'providers/theme_provider.dart';
-import 'screens/home_screen.dart';
-import 'screens/cart_screen.dart';
-import 'screens/add_category_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
+    return MultiProvider(      
       providers: [
         ChangeNotifierProvider(create: (_) => CategoryProvider()),
         ChangeNotifierProvider(create: (_) => ProductProvider()),
         ChangeNotifierProvider(create: (_) => CartProvider()),
         ChangeNotifierProvider(create: (_) => ThemeProvider()),
       ],
-      child: Consumer<ThemeProvider>(
+      
+      child: Consumer<ThemeProvider>(        
         builder: (context, themeProvider, child) {
-          return MaterialApp(
+          return MaterialApp(     
+            debugShowCheckedModeBanner: false,
             title: 'Supermercado',
             theme: ThemeData(
               colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
               useMaterial3: true,
               brightness: themeProvider.isDarkMode ? Brightness.dark : Brightness.light,
             ),
-            home: MainScreen(),
+            home: const MainScreen(),
           );
         },
       ),
